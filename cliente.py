adb38011bc3804fcaa9a9f74e284525131ee1498
Gui.Dev <130806476+DaniloDeademe@users.noreply.github.com>
2024-09-27 19:19:23 -0300

    Correção

---
+++ cliente.py
@@ -0,0 +1,12 @@
+class Cliente:
+    def _init_(self):
+        self.clientes = []  # Lista de clientes
+
+    def cadastrar(self):
+        print('Aqui vai ficar o cadastro de clientes junto do banco de dados')
+
+    def consultar(self):
+        print('Aqui vai ficar a consulta cliente junto com o banco de dados')
+
+    def atualizar(self):
+        print('Aqui sera atualizado o cliente junto com o banco de dados')
